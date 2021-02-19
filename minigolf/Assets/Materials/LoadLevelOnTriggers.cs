ab9338d0802c7400c6f0c1739185e06703ada3e4
Spakelios <73300254+Spakelios@users.noreply.github.com>
2021-02-19 16:30:30 +0000

    issues with moving from one level to the next partially resolved

---
+++ minigolf/Assets/Materials/LoadLevelOnTriggers.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadLevelOnTriggers : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+}
